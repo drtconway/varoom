fb6738365f021384a18fd72402460637e568a91d
Thomas Conway <drtomc@gmail.com>
2019-11-11 08:19:41 +1100

    add a test case for internal empty field.

--- tests/util/test_subtext.cpp
+++ tests/util/test_subtext.cpp
@@ -60,3 +60,25 @@ BOOST_AUTO_TEST_CASE( split1 )
     BOOST_CHECK_EQUAL(static_cast<string>(parts1[5]), string("0"));
     BOOST_CHECK_EQUAL(static_cast<string>(parts1[6]), string(""));
 }
+
+BOOST_AUTO_TEST_CASE( split2 )
+{
+    using namespace std;
+    using namespace varoom;
+
+    string s("18	10037	1	0	0	0		0.00736607");
+    subtext t1(s);
+    
+    vector<subtext> parts1;
+    t1.split('\t', parts1);
+
+    BOOST_CHECK_EQUAL(parts1.size(), 8);
+    BOOST_CHECK_EQUAL(static_cast<string>(parts1[0]), string("18"));
+    BOOST_CHECK_EQUAL(static_cast<string>(parts1[1]), string("10037"));
+    BOOST_CHECK_EQUAL(static_cast<string>(parts1[2]), string("1"));
+    BOOST_CHECK_EQUAL(static_cast<string>(parts1[3]), string("0"));
+    BOOST_CHECK_EQUAL(static_cast<string>(parts1[4]), string("0"));
+    BOOST_CHECK_EQUAL(static_cast<string>(parts1[5]), string("0"));
+    BOOST_CHECK_EQUAL(static_cast<string>(parts1[6]), string(""));
+    BOOST_CHECK_EQUAL(static_cast<string>(parts1[7]), string("0.00736607"));
+}
