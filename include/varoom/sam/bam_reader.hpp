ec7ee15cb0d31b29629b4a51d5e59fafbde1c942
Thomas Conway <thomas.conway@petermac.org>
2019-11-11 17:03:46 +1100

    optimise CIGAR parsing.

--- include/varoom/sam/bam_reader.hpp
+++ include/varoom/sam/bam_reader.hpp
@@ -7,6 +7,9 @@
 
 #include <boost/lexical_cast.hpp>
 
+#define BOOST_LOG_DYN_LINK 1
+#include <boost/log/trivial.hpp>
+
 namespace htslib
 {
 #include <htslib/sam.h>
@@ -18,8 +21,8 @@ namespace varoom
     class bam_reader
     {
     public:
-        bam_reader(const std::string& p_filename)
-            : m_filename(p_filename), m_fp(NULL), m_hdr(NULL), m_aln(NULL), m_more(true)
+        bam_reader(const std::string& p_filename, bool p_verbose = false)
+            : m_filename(p_filename), m_fp(NULL), m_hdr(NULL), m_aln(NULL), m_more(true), m_verbose(p_verbose), m_count(0)
         {
             m_fp = htslib::hts_open(m_filename.c_str(), "r");
             m_hdr = htslib::sam_hdr_read(m_fp);
@@ -52,7 +55,13 @@ namespace varoom
 
         void operator++()
         {
+            static const size_t M = (1 << 20) - 1;
             next();
+            ++m_count;
+            if (m_verbose && more() && (m_count & M) == 0)
+            {
+                BOOST_LOG_TRIVIAL(info) << m_filename << ": " << m_count;
+            }
         }
 
     private:
@@ -105,6 +114,8 @@ namespace varoom
         htslib::bam_hdr_t* m_hdr;
         htslib::bam1_t* m_aln;
         bool m_more;
+        bool m_verbose;
+        size_t m_count;
         sam_alignment m_cur;
     };
 }
--- include/varoom/sam/sam_cigar.hpp
+++ include/varoom/sam/sam_cigar.hpp
@@ -13,17 +13,35 @@ namespace varoom
 
     void decode_cigar(const std::string& p_cigar, std::vector<cigar_op>& p_ops)
     {
-        const std::regex cig_regex("([0-9]+)([DHIMNPSX=])");
-
         p_ops.clear();
-        auto cig_begin = std::sregex_iterator(p_cigar.begin(), p_cigar.end(), cig_regex);
-        auto cig_end = std::sregex_iterator();
-        for (auto itr = cig_begin; itr != cig_end; ++itr)
+        uint32_t n = 0;
+        for (auto itr = p_cigar.begin(); itr != p_cigar.end(); ++itr)
         {
-            std::smatch m = *itr;
-            uint32_t n = boost::lexical_cast<uint32_t>(m[1]);
-            char c = *(m[2].first);
-            p_ops.push_back(cigar_op(c,n));
+            char c = *itr;
+            if ('0' <= c && c <= '9')
+            {
+                n = 10*n + (c - '0');
+            }
+            else
+            {
+                p_ops.push_back(cigar_op(c, n));
+                n = 0;
+            }
+        }
+
+        if (0)
+        {
+            const std::regex cig_regex("([0-9]+)([DHIMNPSX=])");
+
+            auto cig_begin = std::sregex_iterator(p_cigar.begin(), p_cigar.end(), cig_regex);
+            auto cig_end = std::sregex_iterator();
+            for (auto itr = cig_begin; itr != cig_end; ++itr)
+            {
+                std::smatch m = *itr;
+                uint32_t n = boost::lexical_cast<uint32_t>(m[1]);
+                char c = *(m[2].first);
+                p_ops.push_back(cigar_op(c,n));
+            }
         }
     }
 }
