e076bdd6e2b9ec0a449ca1fe684cbadd834ab428
Thomas Conway <thomas.conway@petermac.org>
2020-01-02 15:16:00 +1100

    add a set with rank/select.

---
+++ include/varoom/util/rank_set.hpp
@@ -0,0 +1,352 @@
+#ifndef VAROOM_UTIL_RANK_SET_HPP
+#define VAROOM_UTIL_RANK_SET_HPP
+
+#include <cassert>
+#include <functional>
+#include <memory>
+
+#include <iostream>
+
+namespace varoom
+{
+    namespace detail
+    {
+        template <typename KeyType>
+        struct avl_tree;
+        template <typename KeyType>
+        using avl_tree_ptr = std::shared_ptr<avl_tree<KeyType>>;
+
+        template <typename KeyType>
+        struct avl_tree
+        {
+            using ptr_type = avl_tree_ptr<KeyType>;
+
+            KeyType m_key;
+            size_t m_size;
+            int m_height;
+            ptr_type m_lhs;
+            ptr_type m_rhs;
+
+            avl_tree(const KeyType& p_key)
+                : m_key(p_key), m_size(1), m_height(1)
+            {
+            }
+
+            avl_tree(const KeyType& p_key, ptr_type p_lhs, ptr_type p_rhs)
+                : m_key(p_key), m_lhs(p_lhs), m_rhs(p_rhs)
+            {
+                refresh_size_and_height();
+            }
+
+            size_t size() const
+            {
+                return m_size;
+            }
+
+            size_t height() const
+            {
+                return m_height;
+            }
+
+            bool contains(const KeyType& p_key) const
+            {
+                if (m_key == p_key)
+                {
+                    return true;
+                }
+                else if (p_key < m_key)
+                {
+                    if (m_lhs)
+                    {
+                        return m_lhs->contains(p_key);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (m_rhs)
+                    {
+                        return m_rhs->contains(p_key);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            size_t rank(const KeyType& p_key) const
+            {
+                if (p_key == m_key)
+                {
+                    return (m_lhs ? m_lhs->size() : 0);
+                }
+                if (p_key < m_key)
+                {
+                    return (m_lhs ? m_lhs->rank(p_key) : 0);
+                }
+                else
+                {
+                    return (m_lhs ? m_lhs->size() : 0) + 1 + (m_lhs ? m_rhs->rank(p_key) : 0);
+                }
+            }
+
+            const KeyType& select(size_t p_pos) const
+            {
+                size_t lz = (m_lhs ? m_lhs->size() : 0);
+                if (p_pos < lz)
+                {
+                    return m_lhs->select(p_pos);
+                }
+                p_pos -= lz;
+                if (p_pos == 0)
+                {
+                    return m_key;
+                }
+                else
+                {
+                    return m_rhs->select(p_pos - 1);
+                }
+            }
+
+            void refresh_size_and_height()
+            {
+                m_size = 1;
+                int lh = 0;
+                int rh = 0;
+                if (m_lhs)
+                {
+                    m_size += m_lhs->size();
+                    lh = m_lhs->height();
+                }
+                if (m_rhs)
+                {
+                    m_size += m_rhs->size();
+                    rh = m_rhs->height();
+                }
+                m_height = 1 + std::max(lh, rh);
+            }
+
+            int balance_factor() const
+            {
+                int lh = 0;
+                int rh = 0;
+                if (m_lhs)
+                {
+                    lh = m_lhs->height();
+                }
+                if (m_rhs)
+                {
+                    rh = m_rhs->height();
+                }
+                return lh - rh;
+            }
+
+            static ptr_type rotate_left(ptr_type x)
+            {
+                assert(x);
+                assert(x->m_rhs);
+                ptr_type z = x->m_rhs;
+                ptr_type zl = z->m_lhs;
+                x->m_rhs = zl;
+                x->refresh_size_and_height();
+                z->m_lhs = x;
+                z->refresh_size_and_height();
+                return z;
+            }
+
+            static ptr_type rotate_right(ptr_type x)
+            {
+                assert(x);
+                assert(x->m_lhs);
+                ptr_type z = x->m_lhs;
+                ptr_type zr = z->m_rhs;
+                x->m_lhs = zr;
+                x->refresh_size_and_height();
+                z->m_rhs = x;
+                z->refresh_size_and_height();
+                return z;
+            }
+
+            static ptr_type insert(const KeyType& p_key, ptr_type x)
+            {
+                if (!x)
+                {
+                    return ptr_type(new avl_tree(p_key));
+                }
+
+                if (x->m_key == p_key)
+                {
+                    return x;
+                }
+
+                if (p_key < x->m_key)
+                {
+                    x->m_lhs = insert(p_key, x->m_lhs);
+                }
+                else
+                {
+                    x->m_rhs = insert(p_key, x->m_rhs);
+                }
+
+                x->refresh_size_and_height();
+
+                int b = x->balance_factor();
+
+                assert(-2 <= b && b <= 2);
+
+                if (b > 1 && p_key < x->m_lhs->m_key)
+                {
+                    // left-left
+                    return rotate_right(x);
+                }
+                if (b < -1 && x->m_rhs->m_key < p_key)
+                {
+                    // right-right
+                    return rotate_left(x);
+                }
+                if (b > 1 && x->m_lhs->m_key < p_key)
+                {
+                    // left-right
+                    x->m_lhs = rotate_left(x->m_lhs);
+                    return rotate_right(x);
+                }
+                if (b < -1 && p_key < x->m_rhs->m_key)
+                {
+                    // right-left
+                    x->m_rhs = rotate_right(x->m_rhs);
+                    return rotate_left(x);
+                }
+                return x;
+            }
+
+            static ptr_type min_value_node(ptr_type x)
+            {
+                while (x->m_lhs)
+                {
+                    x = x->m_lhs;
+                }
+                return x;
+            }
+
+            static ptr_type remove(const KeyType& p_key, ptr_type x)
+            {
+                if (!x)
+                {
+                    // Key wasn't there.
+                    return x;
+                }
+
+                if (p_key == x->m_key)
+                {
+                    if (!x->m_lhs || !x->m_rhs)
+                    {
+                        return (x->m_lhs ? x->m_lhs : x->m_rhs);
+                    }
+                    ptr_type t = min_value_node(x->m_rhs);
+                    x->m_key = t->m_key;
+                    x->m_rhs = remove(x->m_key, x->m_rhs);
+                }
+                else if (p_key < x->m_key)
+                {
+                    x->m_lhs = remove(p_key, x->m_lhs);
+                }
+                else
+                {
+                    x->m_rhs = remove(p_key, x->m_rhs);
+                }
+
+                x->refresh_size_and_height();
+                int b = x->balance_factor();
+
+                if (b > 1)
+                {
+                    int lb = (x->m_lhs ? x->m_lhs->balance_factor() : 0);
+                    if (lb < 0)
+                    {
+                        x->m_lhs = rotate_left(x->m_lhs);
+                    }
+                    return rotate_right(x);
+                }
+                if (b < -1)
+                {
+                    int rb = (x->m_rhs ? x->m_rhs->balance_factor() : 0);
+                    if (rb > 0)
+                    {
+                        x->m_rhs = rotate_right(x->m_rhs);
+                    }
+                    return rotate_left(x);
+                }
+                return x;
+            }
+
+            static void sane(ptr_type x)
+            {
+                if (!x)
+                {
+                    return;
+                }
+                sane(x->m_lhs);
+                sane(x->m_rhs);
+                int b = x->balance_factor();
+                assert(-1 <= b && b <= 1);
+            }
+
+            static void visit(ptr_type x, std::function<void(const KeyType&)> p_vis)
+            {
+                if (!x)
+                {
+                    return;
+                }
+                visit(x->m_lhs, p_vis);
+                p_vis(x->m_key);
+                visit(x->m_rhs, p_vis);
+            }
+        };
+    }
+    // namespace detail
+
+    template <typename T>
+    class rank_set
+    {
+    public:
+        rank_set()
+        {
+        }
+
+        size_t size() const
+        {
+            return (m_root ? m_root->size() : 0);
+        }
+
+        size_t rank(const T& p_item) const
+        {
+            return (m_root ? m_root->rank(p_item) : 0);
+        }
+
+        const T& select(size_t p_index) const
+        {
+            assert(p_index < size());
+            return m_root->select(p_index);
+        }
+
+        rank_set& insert(const T& p_item)
+        {
+            m_root = varoom::detail::avl_tree<T>::insert(p_item, m_root);
+        }
+
+        rank_set& erase(const T& p_item)
+        {
+            m_root = varoom::detail::avl_tree<T>::remove(p_item, m_root);
+        }
+
+    private:
+        varoom::detail::avl_tree_ptr<T> m_root;
+    };
+}
+// namespace varoom
+
+#endif // VAROOM_UTIL_RANK_SET_HPP
---
+++ tests/util/test_rank_set.cpp
@@ -0,0 +1,237 @@
+#include "varoom/util/rank_set.hpp"
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE rank_set tests
+#include <boost/test/unit_test.hpp>
+
+#include <random>
+#include <set>
+#include <vector>
+#include <boost/lexical_cast.hpp>
+
+namespace // anonymous
+{
+    using tree_ptr = varoom::detail::avl_tree_ptr<int>;
+    using stree = varoom::detail::avl_tree<std::string>;
+    using stree_ptr = varoom::detail::avl_tree_ptr<std::string>;
+
+    void traverse(tree_ptr x, std::string& r)
+    {
+        if (!x)
+        {
+            r += ".";
+            return;
+        }
+        r += "(";
+        traverse(x->m_lhs, r);
+        r += boost::lexical_cast<std::string>(x->m_key);
+        traverse(x->m_rhs, r);
+        r += ")";
+    }
+}
+// namespace anonymous
+
+BOOST_AUTO_TEST_CASE( rot_1 )
+{
+    std::string v;
+    std::function<void(const int&)> f = [&] (const int& p_x) mutable {
+        v.push_back(p_x);
+    };
+
+    stree_ptr x(new stree("x"));
+    stree_ptr y(new stree("y"));
+    stree_ptr t1(new stree("t1"));
+    stree_ptr t2(new stree("t2"));
+    stree_ptr t3(new stree("t3"));
+    
+    x->m_lhs = t1;
+    x->m_rhs = t2;
+    x->refresh_size_and_height();
+    BOOST_CHECK_EQUAL(x->size(), 3);
+    BOOST_CHECK_EQUAL(x->height(), 2);
+    BOOST_CHECK_EQUAL(x->balance_factor(), 0);
+
+    y->m_lhs = x;
+    y->m_rhs = t3;
+    y->refresh_size_and_height();
+
+    BOOST_CHECK_EQUAL(y->size(), 5);
+    BOOST_CHECK_EQUAL(y->height(), 3);
+    BOOST_CHECK_EQUAL(y->balance_factor(), 1);
+
+    stree_ptr z;
+    z = varoom::detail::avl_tree<std::string>::rotate_right(y);
+    BOOST_CHECK_EQUAL(z.get(), x.get());
+    BOOST_CHECK_EQUAL(x->size(), 5);
+    BOOST_CHECK_EQUAL(x->height(), 3);
+    BOOST_CHECK_EQUAL(x->balance_factor(), -1);
+    BOOST_CHECK_EQUAL(y->size(), 3);
+    BOOST_CHECK_EQUAL(y->height(), 2);
+    BOOST_CHECK_EQUAL(y->balance_factor(), 0);
+
+    z = stree::rotate_left(x);
+    BOOST_CHECK_EQUAL(z.get(), y.get());
+    BOOST_CHECK_EQUAL(x->size(), 3);
+    BOOST_CHECK_EQUAL(x->height(), 2);
+    BOOST_CHECK_EQUAL(x->balance_factor(), 0);
+    BOOST_CHECK_EQUAL(y->size(), 5);
+    BOOST_CHECK_EQUAL(y->height(), 3);
+    BOOST_CHECK_EQUAL(y->balance_factor(), 1);
+}
+
+BOOST_AUTO_TEST_CASE( avl_1 )
+{
+    std::vector<int> v;
+    std::function<void(const int&)> f = [&] (const int& p_x) mutable {
+        v.push_back(p_x);
+    };
+
+    varoom::detail::avl_tree_ptr<int> x;
+    {
+        v.clear();
+        varoom::detail::avl_tree<int>::visit(x, f);
+        BOOST_CHECK_EQUAL(v.size(), 0);
+    }
+    x = varoom::detail::avl_tree<int>::insert(1, x);
+    {
+        v.clear();
+        varoom::detail::avl_tree<int>::visit(x, f);
+        BOOST_REQUIRE_EQUAL(v.size(), 1);
+        BOOST_REQUIRE_EQUAL(v[0], 1);
+    }
+    x = varoom::detail::avl_tree<int>::insert(2, x);
+    {
+        v.clear();
+        varoom::detail::avl_tree<int>::visit(x, f);
+        BOOST_REQUIRE_EQUAL(v.size(), 2);
+        BOOST_REQUIRE_EQUAL(v[0], 1);
+        BOOST_REQUIRE_EQUAL(v[1], 2);
+    }
+    x = varoom::detail::avl_tree<int>::insert(3, x);
+    {
+        v.clear();
+        varoom::detail::avl_tree<int>::visit(x, f);
+        BOOST_REQUIRE_EQUAL(v.size(), 3);
+        BOOST_REQUIRE_EQUAL(v[0], 1);
+        BOOST_REQUIRE_EQUAL(v[1], 2);
+        BOOST_REQUIRE_EQUAL(v[2], 3);
+    }
+
+    std::string r;
+    traverse(x, r);
+    BOOST_CHECK_EQUAL(r, "((.1.)2(.3.))");
+}
+
+BOOST_AUTO_TEST_CASE( avl_2 )
+{
+    std::vector<int> v;
+    std::function<void(const int&)> f = [&] (const int& p_x) mutable {
+        v.push_back(p_x);
+    };
+
+    varoom::detail::avl_tree_ptr<int> x;
+    {
+        v.clear();
+        varoom::detail::avl_tree<int>::visit(x, f);
+        BOOST_CHECK_EQUAL(v.size(), 0);
+    }
+    x = varoom::detail::avl_tree<int>::insert(2, x);
+    {
+        v.clear();
+        varoom::detail::avl_tree<int>::visit(x, f);
+        BOOST_REQUIRE_EQUAL(v.size(), 1);
+        BOOST_REQUIRE_EQUAL(v[0], 2);
+    }
+    x = varoom::detail::avl_tree<int>::insert(0, x);
+    {
+        v.clear();
+        varoom::detail::avl_tree<int>::visit(x, f);
+        BOOST_REQUIRE_EQUAL(v.size(), 2);
+        BOOST_REQUIRE_EQUAL(v[0], 0);
+        BOOST_REQUIRE_EQUAL(v[1], 2);
+    }
+    x = varoom::detail::avl_tree<int>::insert(3, x);
+    {
+        v.clear();
+        varoom::detail::avl_tree<int>::visit(x, f);
+        BOOST_REQUIRE_EQUAL(v.size(), 3);
+        BOOST_REQUIRE_EQUAL(v[0], 0);
+        BOOST_REQUIRE_EQUAL(v[1], 2);
+        BOOST_REQUIRE_EQUAL(v[2], 3);
+    }
+    x = varoom::detail::avl_tree<int>::insert(1, x);
+    {
+        v.clear();
+        varoom::detail::avl_tree<int>::visit(x, f);
+        BOOST_REQUIRE_EQUAL(v.size(), 4);
+        BOOST_REQUIRE_EQUAL(v[0], 0);
+        BOOST_REQUIRE_EQUAL(v[1], 1);
+        BOOST_REQUIRE_EQUAL(v[2], 2);
+        BOOST_REQUIRE_EQUAL(v[3], 3);
+    }
+}
+
+BOOST_AUTO_TEST_CASE( avl_3 )
+{
+    std::vector<int> v;
+    std::function<void(const int&)> f = [&] (const int& p_x) mutable {
+        v.push_back(p_x);
+    };
+
+    varoom::detail::avl_tree_ptr<int> x;
+    size_t N = 1000;
+    std::set<int> S;
+    std::mt19937_64 rng(17);
+    std::uniform_int_distribution<uint64_t> U(0, 1<<30);
+    for (size_t i = 0; i < N; ++i)
+    {
+        int a = U(rng);
+        //std::cerr << "inserting: " << a << std::endl;
+        S.insert(a);
+        x = varoom::detail::avl_tree<int>::insert(a, x);
+
+        v.clear();
+        varoom::detail::avl_tree<int>::visit(x, f);
+        BOOST_REQUIRE_EQUAL(v.size(), S.size());
+        for (size_t j = 1; j < v.size(); ++j)
+        {
+            BOOST_REQUIRE_EQUAL(v[j-1] < v[j], true);
+        }
+
+        std::string r;
+        traverse(x, r);
+        //std::cerr << i << '\t' << r << std::endl;
+    }
+    BOOST_CHECK_EQUAL(x->size(), S.size());
+
+    v.clear();
+    varoom::detail::avl_tree<int>::visit(x, f);
+    BOOST_REQUIRE_EQUAL(v.size(), S.size());
+    size_t i = 0;
+    for (auto s = S.begin(); s != S.end(); ++s, ++i)
+    {
+        BOOST_CHECK_EQUAL(x->contains(*s), true);
+        BOOST_CHECK_EQUAL(x->rank(*s), i);
+        BOOST_CHECK_EQUAL(x->select(i), *s);
+        BOOST_CHECK_EQUAL(v[i], *s);
+    }
+
+    std::shuffle(v.begin(), v.end(), rng);
+    std::vector<int> w(v.begin(), v.end());
+    for (size_t i = 0; i < v.size(); ++i)
+    {
+        int a = v[i];
+        BOOST_CHECK_EQUAL(S.count(a), true);
+        BOOST_CHECK_EQUAL(x->contains(a), true);
+        //std::cerr << i << '\t' << a << std::endl;
+        x = varoom::detail::avl_tree<int>::remove(a, x);
+        varoom::detail::avl_tree<int>::sane(x);
+        S.erase(a);
+        BOOST_CHECK_EQUAL((x ? x->size() : 0), S.size());
+        BOOST_CHECK_EQUAL(S.count(a), false);
+        if (x)
+        {
+            BOOST_CHECK_EQUAL(x->contains(a), false);
+        }
+    }
+}
