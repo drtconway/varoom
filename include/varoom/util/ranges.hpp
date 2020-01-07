ee176b2bf26dc5e4801143f4afc8d40ca3effc25
Thomas Conway <thomas.conway@petermac.org>
2020-01-07 16:43:15 +1100

    work on effient compressed ranges.

--- include/varoom/util/ranges.hpp
+++ include/varoom/util/ranges.hpp
@@ -5,6 +5,9 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <sdsl/bit_vectors.hpp>
+#include <sdsl/rank_support.hpp>
+#include <sdsl/select_support.hpp>
 
 #ifndef VAROOM_UTIL_RANK_SET_HPP
 #include "varoom/util/rank_set.hpp"
@@ -12,15 +15,14 @@
 
 namespace varoom
 {
-    class ranges
+    class ranges_builder
     {
     public:
         using position = size_t;
         using range = std::pair<position,position>;
-        using range_id = size_t;
-        using internal_range_id = size_t;
+        using range_id = uint32_t;
 
-        ranges()
+        ranges_builder()
             : m_next_range_id(0)
         {
         }
@@ -122,6 +124,21 @@ namespace varoom
             }
         }
 
+        const varoom::rank_set<position>& begins() const
+        {
+            return m_begins;
+        }
+
+        const varoom::rank_set<position>& ends() const
+        {
+            return m_ends;
+        }
+
+        const std::unordered_map<position,std::vector<range_id>>& index() const
+        {
+            return m_index;
+        }
+
     private:
         bool in_range(position p_pos) const
         {
@@ -134,6 +151,125 @@ namespace varoom
         varoom::rank_set<position> m_begins;
         varoom::rank_set<position> m_ends;
         std::unordered_map<position,std::vector<range_id>> m_index;
+    };
+
+    class ranges
+    {
+    public:
+        using position = size_t;
+        using range = std::pair<position,position>;
+        using range_id = uint32_t;
+
+        ranges()
+        {
+        }
+
+        void ranges_at(position p_begin, position p_end, std::vector<range_id>& p_ranges) const
+        {
+            p_ranges.clear();
+            size_t r0 = std::min(begins_rank(p_begin), ends_rank(p_begin+1));
+            size_t r1 = std::max(begins_rank(p_end), ends_rank(p_end));
+            size_t i0 = m_index_select(r0+1) - r0;
+            size_t i1 = m_index_select(r1+1) - r1;
+            for (size_t i = i0; i < i1; ++i)
+            {
+                p_ranges.push_back(m_index_ranges[i]);
+            }
+            std::sort(p_ranges.begin(), p_ranges.end());
+            p_ranges.erase(std::unique(p_ranges.begin(), p_ranges.end()), p_ranges.end());
+        }
+
+        void ranges_at(position p_pos, std::vector<range_id>& p_ranges) const
+        {
+            ranges_at(p_pos, p_pos+1, p_ranges);
+        }
+
+        void make(const ranges_builder& p_builder)
+        {
+            make(p_builder.begins(), m_begins, m_begins_rank, m_begins_select);
+            make(p_builder.ends(), m_ends, m_ends_rank, m_ends_select);
+
+            const size_t n = p_builder.begins().size();
+            size_t idx_bits = 1;
+            size_t idx_entries = 0;
+            for (auto itr = p_builder.index().begin(); itr != p_builder.index().end(); ++itr)
+            {
+                //std::cerr << itr->first << std::endl;
+                idx_bits += 1 + itr->second.size();
+                idx_entries += itr->second.size();
+            }
+
+            sdsl::bit_vector B(idx_bits, 0);
+            sdsl::int_vector<32> E(idx_entries, 0);
+
+            size_t j = 0;
+            size_t k = 0;
+            for (size_t i = 0; i < n; ++i)
+            {
+                position p = p_builder.begins().select(i);
+                auto itr = p_builder.index().find(p);
+                B[j++] = 1;
+                j += itr->second.size();
+                for (size_t l = 0; l < itr->second.size(); ++l)
+                {
+                    E[k++] = itr->second[l];
+                }
+            }
+            B[j] = 1;
+
+            m_index.swap(B);
+            m_index_select = sdsl::bit_vector::select_1_type(&m_index);
+            m_index_ranges.swap(E);
+        }
+
+    private:
+        void make(const varoom::rank_set<position>& p_poss,
+                  sdsl::sd_vector<>& p_bits,
+                  sdsl::sd_vector<>::rank_1_type& p_rank,
+                  sdsl::sd_vector<>::select_1_type& p_select)
+        {
+            size_t n = p_poss.size();
+            position m = p_poss.select(n-1);
+
+            sdsl::bit_vector B(m+1, 0);
+            for (size_t i = 0; i < n; ++i)
+            {
+                position p = p_poss.select(i);
+                B[p] = 1;
+            }
+
+            p_bits = sdsl::sd_vector<>(B);
+            p_rank = sdsl::sd_vector<>::rank_1_type(&p_bits);
+            p_select = sdsl::sd_vector<>::select_1_type(&p_bits);
+        }
+
+        size_t begins_rank(position p_pos) const
+        {
+            if (p_pos > m_begins.size())
+            {
+                p_pos = m_begins.size();
+            }
+            return m_begins_rank(p_pos);
+        }
+
+        size_t ends_rank(position p_pos) const
+        {
+            if (p_pos > m_ends.size())
+            {
+                p_pos = m_ends.size();
+            }
+            return m_ends_rank(p_pos);
+        }
+
+        sdsl::sd_vector<> m_begins;
+        sdsl::sd_vector<>::rank_1_type m_begins_rank;
+        sdsl::sd_vector<>::select_1_type m_begins_select;
+        sdsl::sd_vector<> m_ends;
+        sdsl::sd_vector<>::rank_1_type m_ends_rank;
+        sdsl::sd_vector<>::select_1_type m_ends_select;
+        sdsl::bit_vector m_index;
+        sdsl::bit_vector::select_1_type m_index_select;
+        sdsl::int_vector<32> m_index_ranges;
     };
 }
 // namespace varoom
--- tests/util/test_ranges.cpp
+++ tests/util/test_ranges.cpp
@@ -4,19 +4,33 @@
 #define BOOST_TEST_MODULE ranges tests
 #include <boost/test/unit_test.hpp>
 
+#include <nlohmann/json.hpp>
+
 namespace // anonymous
 {
+    void build_chr1(varoom::ranges_builder& R)
+    {
+        std::ifstream i("tests/data/chr1-exons.json");
+        nlohmann::json j;
+        i >> j;
+        for (size_t k = 0; k < j.size(); ++k)
+        {
+            uint64_t p0 = j[k][0];
+            uint64_t p1 = j[k][1];
+            R.insert(varoom::ranges_builder::range(p0, p1));
+        }
+    }
 }
 // namespace anonymous
 
 BOOST_AUTO_TEST_CASE( simple1 )
 {
-    varoom::ranges R;
-    R.insert(varoom::ranges::range(6, 7));
-    R.insert(varoom::ranges::range(1, 4));
-    R.insert(varoom::ranges::range(8, 12));
+    varoom::ranges_builder R;
+    R.insert(varoom::ranges_builder::range(6, 7));
+    R.insert(varoom::ranges_builder::range(1, 4));
+    R.insert(varoom::ranges_builder::range(8, 12));
 
-    std::vector<varoom::ranges::range> rs;
+    std::vector<varoom::ranges_builder::range> rs;
     {
         rs.clear();
         R.overlapping_ranges(0, 10, rs);
@@ -76,14 +90,14 @@ BOOST_AUTO_TEST_CASE( simple1 )
 
 BOOST_AUTO_TEST_CASE( split1 )
 {
-    varoom::ranges R;
-    auto r0 = R.insert(varoom::ranges::range(5, 10));
-    auto r1 = R.insert(varoom::ranges::range(2, 6));
+    varoom::ranges_builder R;
+    auto r0 = R.insert(varoom::ranges_builder::range(5, 10));
+    auto r1 = R.insert(varoom::ranges_builder::range(2, 6));
 
     BOOST_CHECK_EQUAL(r0, 0);
     BOOST_CHECK_EQUAL(r1, 1);
 
-    std::vector<varoom::ranges::range> rs;
+    std::vector<varoom::ranges_builder::range> rs;
     {
         rs.clear();
         R.overlapping_ranges(0, 10, rs);
@@ -101,5 +115,31 @@ BOOST_AUTO_TEST_CASE( split1 )
         BOOST_CHECK_EQUAL(rs[2].second, 10);
         BOOST_REQUIRE_EQUAL(R.ranges_at_segment(6).size(), 1);
         BOOST_CHECK_EQUAL(R.ranges_at_segment(6)[0], 0);
+    }
+}
+
+BOOST_AUTO_TEST_CASE( succinct_ranges_1 )
+{
+    varoom::ranges_builder R;
+    build_chr1(R);
+
+    varoom::ranges r;
+    r.make(R);
+
+    size_t N = R.begins().size();
+    for (size_t i = 0; i < N; ++i)
+    {
+        size_t bb = R.begins().select(i);
+        size_t ee = R.ends().select(i);
+        std::vector<varoom::ranges::range_id> xs;
+        R.ranges_at(bb, ee, xs);
+        std::vector<varoom::ranges::range_id> ys;
+        r.ranges_at(bb, ee, ys);
+
+        BOOST_REQUIRE_EQUAL(ys.size(), xs.size());
+        for (size_t j = 0; j < xs.size(); ++j)
+        {
+            BOOST_CHECK_EQUAL(ys[j], xs[j]);
+        }
     }
 }
